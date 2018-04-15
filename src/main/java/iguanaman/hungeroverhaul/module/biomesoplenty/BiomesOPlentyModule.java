7c9a06347be5a308fcf492508be63ca21d742216
alexbegt <traincrazyb@aol.com>
2018-04-15 19:31:38 -0400

    Fix the crash due to Biomes Of Plenty removing some food items that HungerOverhaul modified the food values on. Closes #185

--- src/main/java/iguanaman/hungeroverhaul/module/biomesoplenty/BiomesOPlentyModule.java
+++ src/main/java/iguanaman/hungeroverhaul/module/biomesoplenty/BiomesOPlentyModule.java
@@ -14,13 +14,8 @@ public class BiomesOPlentyModule
         {
             FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.berries, 1), new FoodValues(1, 0.05F));
             FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.shroompowder, 1), new FoodValues(1, 0.05F));
-            FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.saladfruit, 1), new FoodValues(3, 0.15F));
-            FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.saladveggie, 1), new FoodValues(3, 0.15F));
-            FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.saladshroom, 1), new FoodValues(3, 0.15F));
             FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.persimmon, 1), new FoodValues(1, 0.05F));
             FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.filled_honeycomb, 1), new FoodValues(1, 0.05F));
-            FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.ambrosia, 1), new FoodValues(3, 0.2F));
-            FoodModifier.setModifiedFoodValues(new ItemStack(BOPItems.turnip, 1), new FoodValues(1, 0.05F));
         }
     }
 }
